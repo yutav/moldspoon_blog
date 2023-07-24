70d48ef2fced3fd140673b6e9fc586e1678fa8aa
yutav <tanakayuta2008@gmail.com>
2023-07-25 01:29:58 +0900

    fix

--- lib/components/posts/posts.tsx
+++ lib/components/posts/posts.tsx
@@ -15,7 +15,9 @@ const getMoreLink = (len: number): React.ReactNode => {
   )
 }
 
-const getLatest = (data: typeof metadata, isLatest?: boolean) => {
+const getLatest = (data: typeof metadata, _isLatest?: boolean) => {
+  console.log(_isLatest)
+  const isLatest = false
   const postNode = data.find(item => item.name === 'posts')
   const posts = (postNode || {}).children || []
   if (!isLatest) return posts
@@ -32,10 +34,60 @@ export interface PostsProps {
 }
 
 const Posts: React.FC<PostsProps> = ({ isLatest = false }) => {
+  const theme = useTheme()
+  const posts = useMemo(() => getLatest(metadata, isLatest), [])
+  const title = useMemo(() => getTitle(isLatest), [])
 
   return (
     <section>
-      hogehoge
+      <Head>
+        {!isLatest && (
+          <title>
+            {getTitle(false)} - {Configs.title}
+          </title>
+        )}
+      </Head>
+      <h2>{title}</h2>
+      <div className="content">
+        {posts.map((post, index) => (
+          <PostItem post={post} key={`${post.url}-${index}`} />
+        ))}
+        {isLatest && <span className="more">{getMoreLink(posts.length)}</span>}
+      </div>
+      <style jsx>{`
+        section {
+          margin-top: calc(${theme.layout.gap} * 2);
+        }
+
+        section h2 {
+          font-size: 0.8rem;
+          color: ${theme.palette.accents_6};
+          text-transform: uppercase;
+          letter-spacing: 2px;
+          border-bottom: 2px solid ${theme.palette.accents_6};
+          padding: 2px ${theme.layout.gapQuarter} 0 0;
+          display: inline-block;
+          margin: 0;
+        }
+
+        .content {
+          margin: ${theme.layout.gap} 0;
+        }
+
+        .more {
+          display: block;
+        }
+
+        @media only screen and (max-width: ${theme.layout.breakpointMobile}) {
+          section {
+            margin-top: ${theme.layout.gapQuarter};
+          }
+
+          section h2 {
+            margin-top: calc(1.5 * ${theme.layout.gap});
+          }
+        }
+      `}</style>
     </section>
   )
 }
