8a103271de312939f130a73a5f86c82d18a71208
yutav <tanakayuta2008@gmail.com>
2023-07-25 10:47:45 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Layout, Posts } from 'lib/components'
 
 const Page: React.FC<unknown> = () => (
-  <>hogehoge test</>
+  <Layout>
+    <Posts />
+  </Layout>
 )
 
 export default Page
