92fe6306a8e1ba2ea4671395d8201747fc2e833d
yutav <tanakayuta2008@gmail.com>
2023-07-26 16:27:48 +0900

    sitemap

---
+++ sitemap.config.js
@@ -0,0 +1,21 @@
+const isProd = process.env.NODE_ENV == 'production'
+
+module.exports = {
+  siteUrl: isProd ? 'https://moldspoon.jp/' : 'http://localhost:3001/blog/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public/',
+  exclude: [
+    '/thanks'
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [''], // クローリングしたくないパスがある場合はここに指定します
+      },
+    ],
+  },
+
+};
