fba2c08fc183f5e2c2aa87750e665a010701a963
yutav <tanakayuta2008@gmail.com>
2023-08-02 14:59:11 +0900

    wip

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV == 'production'
 
 module.exports = {
-  siteUrl: isProd ? 'https://moldspoon.jp/' : 'http://localhost:3001/blog/',
+  siteUrl: isProd ? 'https://moldspoon.jp/blog/' : 'http://localhost:3001/blog/',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: './public/',
@@ -13,7 +13,7 @@ module.exports = {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['tools'], // クローリングしたくないパスがある場合はここに指定します
+        disallow: [''], // クローリングしたくないパスがある場合はここに指定します
       },
     ],
   },
