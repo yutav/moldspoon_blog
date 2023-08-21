11405cad88978277e974f3ceeebb3bf18e200d3b
yutav <tanakayuta2008@gmail.com>
2023-08-21 15:12:57 +0900

    TagLink

--- lib/components/original/parts/TagLinks.tsx
+++ lib/components/original/parts/TagLinks.tsx
@@ -6,11 +6,11 @@ type Prop = {
 
 const TagLinks: React.FC<Prop> = ({ tags }) => {
   return (
-    <div className="flex">
+    <div className="flex flex-wrap">
       {tags.map((tag, index) => {
         const encodedTag = encodeURI(tag)
         return (
-          <div className="px-2 mr-2 mb-10 border rounded-lg border-orange-100 text-center" key={index}>
+          <div className="px-2 mr-2 mb-2 md:mb-10 border rounded-lg border-orange-100 text-center" key={index}>
             <Link href={`/tags/${encodedTag}`} target="_blank">
               <span className="text-orange-500 hover:text-orange-300" >
                 {tag}
