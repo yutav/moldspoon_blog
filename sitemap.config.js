ca3994d2ad207682c2daf75ae37733d5d7d3d8d2
yutav <tanakayuta2008@gmail.com>
2023-08-21 13:01:47 +0900

    robots.txtを生成しなくする

--- sitemap.config.js
+++ sitemap.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV == 'production'
 
 module.exports = {
   siteUrl: isProd ? 'https://moldspoon.jp/blog/' : 'http://localhost:3001/blog/',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   sitemapSize: 7000,
   outDir: './public/',
   exclude: [
