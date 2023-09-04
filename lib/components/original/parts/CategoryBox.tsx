6748c0dbd91d06abdd5b1fdade1ad94200614085
yutav <tanakayuta2008@gmail.com>
2023-09-04 21:09:06 +0900

    fix

--- lib/components/original/parts/CategoryBox.tsx
+++ lib/components/original/parts/CategoryBox.tsx
@@ -14,7 +14,7 @@ const CategoryBox: React.FC<Prop> = ({ url, imageUrl, title }) => {
       md:w-32 md:h-32
       lg:w-40 lg:h-40 rounded-xl">
       <Image src={imageUrl} width="160" height="160" alt="note" className="rounded-xl" />
-      <div className="category-title text-center text-sm md:text-2xl text-black font-bold pt-0 h-8 mt-2">
+      <div className="category-title text-center text-sm md:text-2xl text-black dark:text-white font-bold pt-0 h-8 mt-2">
         {title}
       </div>
     </Link>
--- lib/components/original/parts/PrevNext.tsx
+++ lib/components/original/parts/PrevNext.tsx
@@ -25,7 +25,7 @@ const PrevNext: React.FC<Prop> = ({ routerPathName }) => {
               <div className="py-5 w-full grid grid-cols-1 md:grid-cols-2 gap-4 justify-between">
                 {prevPost && (
 
-                  <Link href={prevPost.url} passHref className="border rounded-lg px-4 h-16 md:h-32 text-black text-xs md:text-base  block">
+                  <Link href={prevPost.url} passHref className="border rounded-lg px-4 h-16 md:h-32 text-black dark:text-white text-xs md:text-base  block">
                     <p className="font-bold my-0 md:mt-4 md:mb-2">
                       <i className="ri-arrow-left-circle-fill text-green-700 text-lg"></i>&nbsp;
                       前の記事
@@ -34,7 +34,7 @@ const PrevNext: React.FC<Prop> = ({ routerPathName }) => {
                   </Link>
                 )}
                 {nextPost && (
-                  <Link href={nextPost.url} passHref className="border rounded-lg px-4 h-16 md:h-32 text-black text-right text-xs md:text-base block">
+                  <Link href={nextPost.url} passHref className="border rounded-lg px-4 h-16 md:h-32 text-black dark:text-white text-right text-xs md:text-base block">
                     <p className="font-bold my-0 md:mt-4 md:mb-2">
                       次の記事
                       &nbsp;
