10302182369645d042bd2a0fd90af59396c3fb66
yutav <tanakayuta2008@gmail.com>
2023-09-21 23:30:17 +0900

    fix

--- lib/components/original/parts/IntroduceMyself.tsx
+++ lib/components/original/parts/IntroduceMyself.tsx
@@ -7,10 +7,8 @@ const IntroduceMyself: React.FC<Prop> = () => {
   return (
     <div className="flex">
       <img src={process.env.baseUrl + "/posts/images/yuku_tas_clear.png?n=1"} className="overImage z-50" alt="yuku_tasのアイコン" width={120} height={120} />
-      <div className="bg-gray-200 dark:bg-gray-800 p-3 pl-32 md:p-6 md:pl-32 mr-2 md:mr-0 rounded absolute z-1 mt-5">
-        <div className="icon rounded-full mr-6">
-        </div>
-        <div className="balloon2-left bg-white dark:bg-gray-900 text-black dark:text-white p-3 shadow rounded text-xs md:text-base
+      <div className="bg-gray-200 dark:bg-gray-800 p-3 pl-28 md:pl-32 mr-2 md:mr-0 rounded absolute z-1 mt-5">
+        <div className="balloon2-left bg-white dark:bg-gray-900 text-black dark:text-white px-3 py-2 md:px-4 md:py-3 shadow rounded text-xs md:text-base
         h-16 md:h-auto
         before:border-r-4 before:dark:border-r-white
         after:border-r-5 after:dark:border-r-white
@@ -22,14 +20,13 @@ const IntroduceMyself: React.FC<Prop> = () => {
         <style jsx>{`
 
        .balloon2-left {
-        position: relative;
-        display: inline-block;
-        padding: 7px 10px;
-        min-width: 120px;
-        max-width: 100%;
-        border: solid 3px #555;
-        box-sizing: border-box;
-      }
+          position: relative;
+          display: inline-block;
+          min-width: 120px;
+          max-width: 100%;
+          border: solid 3px #555;
+          box-sizing: border-box;
+       }
       
       .balloon2-left:before {
         content: "";
@@ -38,6 +35,7 @@ const IntroduceMyself: React.FC<Prop> = () => {
         left: -24px;
         margin-top: -12px;
         border: 12px solid transparent;
+        border-right: 12px solid #FFF;
         z-index: 2;
       }
       
