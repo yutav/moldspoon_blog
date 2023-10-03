1086a5f3504930f3824aa4f95a9936eb8c0fe915
yutav <tanakayuta2008@gmail.com>
2023-10-03 14:46:17 +0900

    importを一部置換。省力化記事を書いた

---
+++ lib/components/original/parts/ThanksComment.tsx
@@ -0,0 +1,8 @@
+const ThanksComment = () => {
+  return (
+    <>この記事が何かのお役に立てれば幸いです。<br />
+      最後までお読みいただきありがとうございました！</>
+  )
+}
+
+export default ThanksComment
---
+++ lib/components/original/parts/index.ts
@@ -0,0 +1,5 @@
+export { default as RichLink } from './RichLink';
+export { default as RichList } from './RichList';
+export { default as RichListNoBox } from './RichListNoBox';
+export { default as CodeBlockTitle } from './CodeBlockTitle';
+export { default as MdxImage } from './MdxImage';
