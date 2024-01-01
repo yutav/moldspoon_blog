947ab8b57bf9232820f88b9ea4ccfe3ba3bc5649
yutav <tanakayuta2008@gmail.com>
2024-01-02 00:49:56 +0900

    fix css path

--- lib/components/layout.tsx
+++ lib/components/layout.tsx
@@ -86,7 +86,7 @@ const LayoutHeader: React.FC<LayoutHeader> = ({ isDetailPage, currentUrl, meta }
         <Script strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1104475365452915" crossOrigin="anonymous" />
       )}
-      <link rel="stylesheet" href="https://unpkg.com/@speed-highlight/core/dist/themes/default.css" />
+      <link rel="stylesheet" href={`${process.env.baseUrl}/css/speed-highlight-1.2.4/default.css`} />
 
     </Head >
   )
