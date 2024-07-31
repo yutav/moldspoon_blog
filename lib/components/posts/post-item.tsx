74620ce559ebf8b4643e1d15d52c57b1215d867f
yutav <tanakayuta2008@gmail.com>
2024-07-31 10:58:59 +0900

    fix

--- lib/components/posts/post-item.tsx
+++ lib/components/posts/post-item.tsx
@@ -31,12 +31,12 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
       <div className="flex">
         <Link href={post.url} passHref className="flex-none">
           {isMedium ? (
-            <Image src={process.env.baseUrl + "/api/og?title=" + encodeURI(post.name)} width="100" height="44" alt={post.name} style={{
+            <Image src={process.env.baseUrl + "/api/og?v=4&title=" + encodeURI(post.name)} width="100" height="44" alt={post.name} style={{
               borderRadius: '5px'
             }} className='hover:opacity-80'
             />
           ) : (
-            <Image src={process.env.baseUrl + "/api/og?title=" + encodeURI(post.name)} width="320" height="140" alt={post.name} style={{
+            <Image src={process.env.baseUrl + "/api/og?v=3&title=" + encodeURI(post.name)} width="320" height="140" alt={post.name} style={{
               borderRadius: '12.5px'
             }} className='hover:opacity-80'
             />
